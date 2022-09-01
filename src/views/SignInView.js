057c5360db577ad73b173f5aafe02cf4cec64af6
MagnusThiemer <thiemer.jensen@gmail.com>
2022-09-01 11:55:05 +0200

    add empty view templates

---
+++ src/views/SignInView.js
@@ -0,0 +1,5 @@
+const SignInView = () => {
+  return ( <div></div> );
+}
+ 
+export default SignInView;
---
+++ src/views/WelcomeView.js
@@ -0,0 +1,9 @@
+const WelcomeView = () => {
+  return ( 
+    <div>
+      
+    </div>
+   );
+}
+ 
+export default WelcomeView;
